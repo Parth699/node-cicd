25a40ad345d9383b511b356755aa1806cb841a36
Parth699 <parthvasani678@gmail.com>
2024-07-18 17:23:08 +0530

    Adds /tem endpoint.

--- src/server.js
+++ src/server.js
@@ -17,7 +17,11 @@ app.get('/info',(req,res)=>{
         port: 80,
         description: 'This is a simple node express api with ci/cd using github actions with hosted runner in ec2.'
     });
-    
+
+})
+
+app.get('/tem',(req,res)=>{
+    res.status(200).json({msg:'Temporary endpoint.'});
 })
 
 module.exports={app};
